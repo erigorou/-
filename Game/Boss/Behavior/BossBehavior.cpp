6bcab350ad92a8f45a53c3f13b0b07199a84d0c4
erigorou <139610310+erigorou@users.noreply.github.com>
2025-06-16 14:38:12 +0900

    ビヘイビアツリーの仮組完成

--- Game/Boss/Behavior/BossBehavior.cpp
+++ Game/Boss/Behavior/BossBehavior.cpp
@@ -57,6 +57,12 @@ void BossBehavior::Initialize()
 /// </summary>
 void BossBehavior::Finalize()
 {
+	// ルートノードの削除
+	m_rootNode.reset();
+	// 実行ノードの削除
+	m_executionNode.reset();
+	// 条件ノードの削除
+	m_conditionNode.reset();
 }
 
 
@@ -76,14 +82,115 @@ void BossBehavior::CreateBehaviorTree()
 	// HPが半分以上の場合のビヘイビアの構築
 	//___________________________________________
 
-	// 叩きつけ攻撃のデコレーター
-	auto attackDecoratorNode = 
+	// ★攻撃★
+	// 1/2　→　叩きつけ
+	auto attackDecoratorNode = CreateHalfSuccessDecorator();
+	attackDecoratorNode->AddNode(CreateAttackActionNode());
+	// 1/2　→　薙ぎ払い
+	auto sweepDecoratorNode = CreateHalfSuccessDecorator();
+	sweepDecoratorNode->AddNode(CreateSweepActionNode());
+	// 待機
+	auto idleActionNode = CreateIdleActionNode();
+
+	// 攻撃セレクター
+	auto attackSelectorNode = std::make_unique<SelectorNode>();
+	// 叩きつけデコレータを追加
+	attackSelectorNode->AddNode(std::move(attackDecoratorNode));
+	// 薙ぎ払いデコレータを追加
+	attackSelectorNode->AddNode(std::move(sweepDecoratorNode));
+	// 待機アクションを追加
+	attackSelectorNode->AddNode(std::move(idleActionNode));
 
 
 
+	// ★近づく★
+	// 1/2　→　近づく
+	auto approachActionNode = CreateHalfHpDecorator();
+	approachActionNode->AddNode(CreateApproachActionNode());
+	// ダッシュ攻撃
+	auto dashActionNode = CreateDashActionNode();
+
+	// 近づくセレクター
+	auto approachSelectorNode = std::make_unique<SelectorNode>();
+	// 近づくアクションを追加
+	approachSelectorNode->AddNode(std::move(approachActionNode));
+	// ダッシュ攻撃アクションを追加
+	approachSelectorNode->AddNode(std::move(dashActionNode));
+
+
+	// ★HPが半分以上★
+	// 攻撃範囲内にいるなら攻撃セレクター、違うなら近づくセレクターを実行するセレクター
+	auto inAttackRangeDecoratorNode = CreateInAttackRangeDecorator();
+	inAttackRangeDecoratorNode->AddNode(std::move(attackSelectorNode));
+
+	auto halfHpOverSelectorNode = std::make_unique<SelectorNode>();
+	// 攻撃範囲内にいるなら攻撃セレクターを追加
+	halfHpOverSelectorNode->AddNode(std::move(inAttackRangeDecoratorNode));
+	// 攻撃範囲外なら近づくセレクターを追加
+	halfHpOverSelectorNode->AddNode(std::move(approachSelectorNode));
+	
+
+
+	//___________________________________________
+	//
+	// HPが半分以下の場合のビヘイビアの構築
+	//___________________________________________
+
+	// ★攻撃★
+	
+	// 薙ぎ払いアクションを追加
+	sweepDecoratorNode = CreateHalfSuccessDecorator();
+	sweepDecoratorNode->AddNode(CreateSweepActionNode());
+	// 叩きつけアクションを追加
+	attackDecoratorNode = CreateHalfSuccessDecorator();
+	attackDecoratorNode->AddNode(CreateAttackActionNode());
+
+	// 攻撃セレクター
+	auto attackSelectorNode2 = std::make_unique<SelectorNode>();
+	// 薙ぎ払いデコレータを追加
+	attackSelectorNode2->AddNode(std::move(sweepDecoratorNode));
+	// 叩きつけデコレータを追加
+	attackSelectorNode2->AddNode(std::move(attackDecoratorNode));
+
+
+	// ★近づく★
+	// 近づくアクションを追加
+	auto approachDecoratorNode = CreateHalfSuccessDecorator();
+	approachDecoratorNode->AddNode(CreateApproachActionNode());
+
+	// ダッシュ攻撃アクションを追加
+	auto dashDecoratorNode = CreateHalfSuccessDecorator();
+	dashDecoratorNode->AddNode(CreateDashActionNode());
+
+
+	// ★HPが半分以下★
+	// 攻撃範囲内にいるなら攻撃セレクター、違うなら近づくセレクターを実行するセレクター
+	auto inAttackRangeDecoratorNode2 = CreateInAttackRangeDecorator();
+	inAttackRangeDecoratorNode2->AddNode(std::move(attackSelectorNode2));
+
+	// HP半分以下のセレクター
+	auto halfHpUnderSelectorNode = std::make_unique<SelectorNode>();
+	// 攻撃範囲内にいるなら攻撃セレクターを追加
+	halfHpUnderSelectorNode->AddNode(std::move(inAttackRangeDecoratorNode2));
+	// 攻撃範囲外なら近づくセレクターを追加
+	halfHpUnderSelectorNode->AddNode(std::move(approachDecoratorNode));
 
 
 
+	//_____________________________________________________
+	//
+	// HPが半分以上か以下かで分岐するノードの作成
+	//_____________________________________________________
+
+	// ボスのHPが半分以上かどうかで分岐するセレクター
+	auto selectorNode = std::make_unique<SelectorNode>();
+	selectorNode->AddNode(std::move(halfHpOverSelectorNode));
+	selectorNode->AddNode(std::move(halfHpUnderSelectorNode));
+
+	// ルートノードの作成
+	m_rootNode = std::make_unique<Root>();
+	// ルートノードにセレクターを追加
+	m_rootNode->AddNode(std::move(selectorNode));
 
 
 	// 状態の初期化
@@ -92,7 +199,26 @@ void BossBehavior::CreateBehaviorTree()
 
 
 
+/// <summary>
+/// ビヘイビアを動かす
+/// </summary>
+void BossBehavior::RunBehavior()
+{
+	switch (m_currentState)
+	{
+	case IBehaviorNode::NodeState::SUCCESS:
+		break;
+	case IBehaviorNode::NodeState::FAILURE:
+		m_rootNode->Update(0.0f);
+		break;
+	default:
+		break;
+	}
+}
 
+
+
+// ----------------------------------------------------------------------
 
 
 
@@ -119,8 +245,7 @@ std::unique_ptr<ActionNode> BossBehavior::CreateIdleActionNode()
 std::unique_ptr<ActionNode> BossBehavior::CreateAttackActionNode()
 {
 	// 叩きつけアクションノードを作成
-	return std::make_unique<ActionNode>(
-		std::bind(&ExecutionNode::BossEnemyAttackAction, m_executionNode.get()));
+	return std::make_unique<ActionNode>(std::bind(&ExecutionNode::BossEnemyAttackAction, m_executionNode.get()));
 }
 
 
@@ -158,10 +283,14 @@ std::unique_ptr<ActionNode> BossBehavior::CreateApproachActionNode()
 
 
 
+// ----------------------------------------------------------------------
+
+
 
 ///////////////////////////////////////////////////////////////////////
 // ★ == コンディションを持ったデコレータノードを作成するクラス群 == ★
 ///////////////////////////////////////////////////////////////////////
+
 
 /// <summary>
 /// 2分の1で成功するデコレーターを作成
@@ -169,7 +298,7 @@ std::unique_ptr<ActionNode> BossBehavior::CreateApproachActionNode()
 /// <returns>デコレーター</returns>
 std::unique_ptr<DecoratorNode> BossBehavior::CreateHalfHpDecorator()
 {
-	return std::unique_ptr<DecoratorNode>();
+	return std::make_unique<DecoratorNode>(std::bind(&BossCondition::IsHalfSuccess, m_conditionNode.get()));
 }
 
 
@@ -179,7 +308,7 @@ std::unique_ptr<DecoratorNode> BossBehavior::CreateHalfHpDecorator()
 /// <returns>デコレーター</returns>
 std::unique_ptr<DecoratorNode> BossBehavior::CreateInAttackRangeDecorator()
 {
-	return std::unique_ptr<DecoratorNode>();
+	return std::make_unique<DecoratorNode>(std::bind(&BossCondition::IsInCloseRange, m_conditionNode.get()));
 }
 
 
@@ -189,5 +318,5 @@ std::unique_ptr<DecoratorNode> BossBehavior::CreateInAttackRangeDecorator()
 /// <returns>デコレーター</returns>
 std::unique_ptr<DecoratorNode> BossBehavior::CreateHalfSuccessDecorator()
 {
-	return std::unique_ptr<DecoratorNode>();
+	return std::make_unique<DecoratorNode>(std::bind(&BossCondition::IsHpOverHalf, m_conditionNode.get()));
 }
